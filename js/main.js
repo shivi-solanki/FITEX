9873341d03b2b21f51a9225e42c40179d33050b8
shivi-solanki <51690897+shivi-solanki@users.noreply.github.com>
2021-05-25 15:29:03 +0530

    Shivani

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const openNav = () => {
+            document.getElementById('mysidenav').style.width = '250px';
+            document.getElementById('mysidenav').style.padding= '70px 60px';
+
+}
+const closeNav = () => {
+              document.getElementById('mysidenav').style.width = '0px';
+              document.getElementById('mysidenav').style.padding = '70px 0px';
+}
